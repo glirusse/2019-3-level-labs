91a15405085fbb84264a64f7aefb9da082bec1a2
glirusse <36458651+glirusse@users.noreply.github.com>
2019-10-17 03:27:52 +0300

    Update tests.py

--- lab_1/lr1/tests/tests.py
+++ lab_1/lr1/tests/tests.py
@@ -3,14 +3,6 @@ import requests
 from lab_1.lr1.crawl import valid
 
 
-def valid(url):
-    r1 = requests.get(url)
-    if r1:
-        return r1
-    else:
-        print('invalid URL')
-
-
 class TestCrawler(unittest.TestCase):
     def setUp(self):
         self.url = 'https://3dnews.ru/'
