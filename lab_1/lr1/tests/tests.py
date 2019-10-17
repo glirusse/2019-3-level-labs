b894719fc8026ec1e5eb026f7cef7aa972d71963
Yurido22 <55536411+Yurido22@users.noreply.github.com>
2019-10-17 15:16:24 +0300

    Update tests.py

--- lab_1/lr1/tests/tests.py
+++ lab_1/lr1/tests/tests.py
@@ -1,10 +1,11 @@
 import unittest
+import requests
 from lab_1.lr1.crawl import valid, invalid
 
 class TestCrawler(unittest.TestCase):
     def setUp(self):
         self.url = 'https://3dnews.ru/'
-        self.invalid_url = 'https://3dnews.ru/asdassaaa'
+        self.invalid_url = 'https://3dnews.ru/assalamusalla'
 
     def test_valid(self):
         self.assertEqual(valid(self.url).status_code, 200)
