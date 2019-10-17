99580a1977976e013b2da4057658afc603becc30
Yurido22 <55536411+Yurido22@users.noreply.github.com>
2019-10-17 14:45:48 +0300

    Update tests.py

--- lab_1/lr1/tests/tests.py
+++ lab_1/lr1/tests/tests.py
@@ -4,6 +4,10 @@ from lab_1.lr1.crawl import valid
 class TestCrawler(unittest.TestCase):
     def setUp(self):
         self.url = 'https://3dnews.ru/'
+        self.incorrect_url = 'https://3dnews.ru/asdassaaa'
 
     def test_valid(self):
         self.assertEqual(valid(self.url).status_code, 200)
+    
+    def test_valid(self):
+        self.assertEqual(valid(self.incorrect_url).status_code, 404)
